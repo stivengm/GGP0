aec3b5c2ddeeec982012f3f86f1e0411512cd0ee
Stiven Gómez <stivenalexander7u7@gmail.com>
2018-10-18 09:18:47 -0500

    Fixed dataProfile, key variable.

--- src/app/data-profile.service.ts
+++ src/app/data-profile.service.ts
@@ -8,12 +8,14 @@ import { from } from 'rxjs';
 })
 export class DataProfileService {
 
+  key = 'RGAPI-1e3dc4ef-bb80-4253-a368-6c0a51a31339';
+
   constructor(private httpClient: HttpClient) {
     console.log("Se cosumirá un servicio.");
   }
 
   getData() {
-    return this.httpClient.get('https://la1.api.riotgames.com/lol/summoner/v3/summoners/by-name/XGameGamePlay0X?api_key=RGAPI-1e3dc4ef-bb80-4253-a368-6c0a51a31339');
+    return this.httpClient.get('https://la1.api.riotgames.com/lol/summoner/v3/summoners/by-name/XGameGamePlay0X?api_key='+ this.key);
   }
 
 }
