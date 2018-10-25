a0c0afcb2b19097adacf0086f1eb194ffb960a9b
Stiven Gómez <stivenalexander7u7@gmail.com>
2018-10-25 16:29:51 -0500

    Fix championComponent.

--- src/app/components/champs/champions/champions.component.ts
+++ src/app/components/champs/champions/champions.component.ts
@@ -9,7 +9,7 @@ export class ChampionsComponent implements OnInit {
 
   champions = [
     {championsId: 'aatrox', history: 'Once honored defenders of Shurima against the Void, Aatrox and his brethren would eventually become an even greater threat to Runeterra, and were defeated only by cunning mortal sorcery. But after centuries of imprisonment, Aatrox was the first to find freedom once more, corrupting and transforming those foolish enough to try and wield the magical weapon that contained his essence. Now, with stolen flesh, he walks Runeterra in a brutal approximation of his previous form, seeking an apocalyptic and long overdue vengeance.'},
-    {championsId: 'ahri'},
+    {championsId: 'ahri', history: 'Once honored defenders of Shurima against the Void, Aatrox and his brethren would eventually become an even greater threat to Runeterra, and were defeated only by cunning mortal sorcery. But after centuries of imprisonment, Aatrox was the first to find freedom once more, corrupting and transforming those foolish enough to try and wield the magical weapon that contained his essence. Now, with stolen flesh, he walks Runeterra in a brutal approximation of his previous form, seeking an apocalyptic and long overdue vengeance.'},
     {championsId: 'akali'},
     {championsId: 'alistar'},
     {championsId: 'amumu'},
