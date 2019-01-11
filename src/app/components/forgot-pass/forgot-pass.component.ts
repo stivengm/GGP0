24d8835f22f1bb9f1e0f35a68c7b0b6c77765112
Stiven Gómez <stivenalexander7u7@gmail.com>
2019-01-11 15:50:05 -0500

    fix styles forgot and design forgot.

--- src/app/components/forgot-pass/forgot-pass.component.ts
+++ src/app/components/forgot-pass/forgot-pass.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-forgot-pass',
   templateUrl: './forgot-pass.component.html',
-  styleUrls: ['./forgot-pass.component.css']
+  styleUrls: ['./forgot-pass.component.scss']
 })
 export class ForgotPassComponent implements OnInit {
   title = 'GGP0'
